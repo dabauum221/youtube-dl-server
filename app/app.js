32afe8d2c582d6074767aebb7cd2412f76b6d5d8
Daniel Baum <daniel.m.baum@gmail.com>
2018-07-26 02:44:57 +0000

    Adding style and initial functionality to form

--- app/app.js
+++ app/app.js
@@ -1,12 +1,22 @@
 /*eslint-env node*/
 
+const bodyParser = require('body-parser');
 var express = require('express');
 var app = express();
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
+
 
 app.get('/', function (req, res) {
   res.render('index');
 });
+
+app.post('/', function (req, res) {
+  res.render('index');
+  console.log('City: ' + req.body.city);
+})
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
