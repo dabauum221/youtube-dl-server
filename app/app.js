b50bcb1c5f2bf4221023e878d253f6e233d25d3e
Daniel Baum <daniel.m.baum@gmail.com>
2018-07-26 02:12:55 +0000

    Adding run configurations

--- app/app.js
+++ app/app.js
@@ -2,9 +2,10 @@
 
 var express = require('express');
 var app = express();
+app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.render('index');
 });
 
 app.listen(3000, function () {
