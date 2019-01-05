e08b8b04718334447e96228c35fb14ddca84bbb7
Daniel Baum <daniel.m.baum@gmail.com>
2019-01-05 20:24:05 +0000

    Starting to migrate to a fully function rest API to provide different YouTube video functionality

---
+++ app/routes.js
@@ -0,0 +1,45 @@
+// Set up the variables =========================================================
+var youtubedl = require('youtube-dl');
+
+// Register the API routes
+module.exports = function (app) {
+    
+    // Test root ----------------------------------------------------------------
+    app.get('/', function (req, res) {
+        res.send('test');
+    });
+    
+    // Get info from a YouTube video
+    app.get('/api/info', function (req, res) {
+        // console.log(req);
+        // Respond with the video info as JSON
+        // youtubedl.getInfo(req.body.url, function(err, info) {
+            // if (err) ;
+            // res.send(info);
+        // });
+        res.send('debug');
+    });
+    
+    // Get info from a YouTube video
+    app.get('/api/info/:url', function (req, res) {
+        console.log(req.params);
+        // Respond with the video info as JSON
+        youtubedl.getInfo(req.params.url, function(err, info) {
+            // if (err) ;
+            res.send(info);
+        });
+    });
+    
+    // Download a YouTube video
+    app.get('/api/download/:url/:format', function (req, res) {
+        var options = ['--format=' + req.params.format];
+        var video = youtubedl(req.params.url, options);
+        // Will be called when the download starts.
+        video.on('info', function(info) {
+            res.header('Content-Disposition', 'attachment; filename="' + encodeURIComponent(info._filename) + '"');
+            res.header('Content-Type', 'video/mp4');
+            res.header('Content-Length', info.size);
+            video.pipe(res);
+        });
+    });
+};
--- server.js
+++ server.js
@@ -1,52 +1,21 @@
-/* eslint-env node */
-
-const bodyParser = require('body-parser');
+// Set up the variables =========================================================
 var express = require('express');
-var favicon = require('serve-favicon');
+var bodyParser = require('body-parser');
 var path = require('path');
-// var fs = require('fs');
-var youtubedl = require('youtube-dl');
+var port = process.env.PORT || 3000;
+var favicon = require('serve-favicon');
 var app = express();
-// var spawn = require('child_process').spawn;
-// var pattern = /[0-9]+\.[0-9]%/gi;
 
-app.use(express.static('public'));
+// Configuration ================================================================
+app.use(express.static('./public')); // set the static files location /public/img will be /img for users
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json()); // parse application/json
 app.use(favicon(path.join(__dirname,'public','images','favicon.ico')));
-app.set('view engine', 'ejs');
 
-app.get('/', function (req, res) {
-  res.render('index');
-});
+// Routes =======================================================================
+require('./app/routes.js')(app);
 
-app.post('/', function (req, res) {
-  //res.render('index');
-  console.log('URL: ' + req.body.url);
-  
-  // Get video info
-  youtubedl.getInfo(req.body.url, function(err, info) {
-    if (err) throw err;
-    console.log('info:', info);
-  });
-  var options = ['--format=best'];
-  var video = youtubedl(req.body.url, options);
-  // Will be called when the download starts.
-  video.on('info', function(info) {
-    console.log('Download started');
-    console.log('filename: ' + info._filename);
-    console.log('size: ' + info.size);
-    res.header('Content-Disposition', 'attachment; filename="' + encodeURIComponent(info._filename) + '"');
-    res.header('Content-Type', 'video/mp4');
-    res.header('Content-Length', info.size);
-    video.pipe(res);
-  });
-  // Will be called when the download ends.
-  video.on('end', function() {
-    console.log('Finished downloading!');
-  });
-  // video.pipe(fs.createWriteStream('/tmp/' + filename));
-});
-
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+// Listen =======================================================================
+app.listen(port, function () {
+    console.log('Application listening on port ' + port);
 });
